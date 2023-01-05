a5f9a8d3dfa9cddbea3a9f326e1db14ad6d87ce2
yueh0607 <imyueh@foxmail.com>
2023-01-05 15:14:36 +0800

    init

---
+++ node_menu.h
@@ -0,0 +1,191 @@
+#pragma once
+// ReSharper disable All
+#include<iostream>
+#include<vector>
+#include<cassert>
+#include"framework.h"
+#include<windows.h>
+using namespace std;
+
+
+class Menu;
+class Node :public Instance
+{
+private:
+	int lev = 0;//节点层级，根节点为0
+public:
+	Node* parent = nullptr; //父节点指针，根节点为nullptr
+	void (*func)(Menu& munu, Canvas& canvas) = nullptr; //节点功能函数，循环执行
+	void (*onceFunc)(Menu& munu) = nullptr; //节点功能辅助函数，只执行一次
+	string tag; //节点名
+	vector<Node*> childs; //子节点指针表
+	bool funcNode = false;  //是否为单一功能节点
+
+	//当前节点层级
+	int Level() { return lev; }
+
+	
+#pragma region 节点构造
+	//构造/析构
+	Node()
+	{
+		tag = "RootNode";
+		lev = 0;
+	}
+	//形参：父节点指针，节点名，节点功能函数(循环)，是否为单一功能节点，辅助函数(只执行一次)
+	Node(Node* p, const char* t, void f(Menu& menu, Canvas& canvas) = nullptr, bool fnode = false, void once(Menu& menu) = nullptr)
+	{
+		assert(p != nullptr);
+		parent = p;
+		func = f;
+		tag = t;
+		funcNode = fnode;
+		lev = p->lev + 1;
+		onceFunc = once;
+		parent->childs.push_back(this);
+	}
+	//DFS析构
+	~Node()
+	{
+		if (childs.size() == 0) return;
+		for (auto i : childs) delete i;
+	}
+	
+#pragma endregion
+#pragma region 节点切换
+	//尝试跳转到下一节点
+	Node* Next(int id, bool* result=NULL)
+	{
+		assert(id > 0);  
+		if (id >= 1 && id <= childs.size())
+		{
+			if (result != NULL)*result = true;
+			return childs[id - 1];
+		}
+		if (result != NULL)*result = false;
+		return this;
+	}
+	//尝试返回上一节点
+	Node* Last(bool* result = NULL)
+	{
+		if (parent != nullptr)
+		{
+			if (result != NULL)*result = true;
+			return parent;
+		}
+		if (result != NULL)*result = false;
+		return this;
+	}
+#pragma endregion
+};
+
+//节点菜单类，不负责具体逻辑，只负责维护节点树
+class Menu :public Instance
+{
+	Node* root;  //根节点
+	
+	Menu() = default;
+public:
+	//当前节点,虽然可以被修改了，但是不管了....累
+	Node* current;
+	Canvas* canvas;//画布
+
+	//构造/析构
+	Menu(Node* first, Canvas* cv)
+	{
+		root = first;  //根节点
+		canvas = cv;  //画布
+		current = first;  //当前节点
+
+	}
+	~Menu()
+	{
+		//在根节点DFS回收整个N叉树
+		delete root;
+	}
+#pragma region 节点跳转函数
+
+	//节点函数，负责节点跳转，如果不允许跳转，则current不变，每次跳转都调用节点更新辅助函数
+	void ToRoot()
+	{
+		current = root;
+		if (current->onceFunc != nullptr) current->onceFunc(*this);
+	}
+	//节点函数，负责节点跳转，如果不允许跳转，则current不变，每次跳转都调用节点更新辅助函数
+	void Last()
+	{
+		current = current->Last();
+		if (current->onceFunc != nullptr) current->onceFunc(*this);
+	}
+	//节点函数，负责节点跳转，如果不允许跳转，则current不变，每次跳转都调用节点更新辅助函数
+	void Next(int idx)
+	{
+		current = current->Next(idx);
+		if (current->onceFunc != nullptr) current->onceFunc(*this);
+	}
+
+#pragma endregion
+};
+//输入组，负责一次性读取控制台输入
+class InputGroup
+{
+private:
+	COORD coord;
+	HANDLE handle;
+	struct KeyValuePair
+	{
+		string key;
+		bool value;
+		char* format;
+		void* p;
+	};
+	vector<KeyValuePair> pool;
+	bool nowarning = false;
+public:
+	InputGroup(int initY, bool warnning = true)
+	{
+		handle = GetStdHandle(STD_OUTPUT_HANDLE);
+		coord.X = 0;
+		coord.Y = initY;
+		nowarning = !warnning;
+	}
+	void Add(const string& str, const char* formatter, void* pp)
+	{
+
+		pool.push_back({ str,true,(char*)formatter,pp });
+	}
+	void Add(const string& str)
+	{
+		pool.push_back({ str,false });
+	}
+
+	void Print()
+	{
+		for (auto& i : pool)
+		{
+			cout << i.key << endl;
+		}
+	}
+	bool GetInput()
+	{
+		for (auto& i : pool)
+		{
+			if (i.value)
+			{
+				coord.X = i.key.length();
+				SetConsoleCursorPosition(handle, coord);
+
+				int r = scanf(i.format, i.p);
+				if (r == 0 && !nowarning)
+				{
+					if (!nowarning)MessageBox(NULL, ("输入格式错误"), ("请检查您的输入并重新尝试"), MB_SYSTEMMODAL | MB_ICONSTOP);
+					return false;
+				}
+
+			}
+			coord.Y++;
+		}
+		return true;
+	}
+};
+
